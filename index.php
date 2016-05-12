358a883fe96ff2c8dfaffa6fa9e2cfb4b8d96cdc
Charles Sandidge <sandidgec@gmail.com>
2016-05-12 08:46:11 -0600

    added index php and gitignore

---
+++ index.php
@@ -0,0 +1,11 @@
+<doctype HTML>
+    <html>
+        <head>
+
+        </head>
+    <body>
+        <h1>Hello Cultivating Coders Students!</h1>
+    <h2>Lets test the connnection</h2>
+    </body>
+
+    </html>
